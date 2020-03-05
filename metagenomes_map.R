47ba754dfae9ff9495f3f56142bca05e801295ca
wfrancis <wfrancis@biology.sdu.dk>
2020-03-05 11:13:40 +0100

    added some user notes

--- metagenomes_map.R
+++ metagenomes_map.R
@@ -16,6 +16,37 @@ longitude = metagenomedata[["V7"]]
 # remove metagenome from the name of each
 metagenome_type = sub(" metagenome", "", metagenomedata[["V11"]])
 
+
+# NOTES on categories
+#
+# humancols       is defined as all human, excluding gut or feces
+# gutscols        is guts from all other animals
+# miscbodycols    is undefined organisms, but is treated as human, possibly mislabeled
+# mar_animalcols  is marine animals, though includes freshwater animals, and possibly some land
+#                 includes some unclear terms: "mollusc" (could be snail/slug?), "annelid" (same), "crustacean" (could be land isopods?)
+#                 "marine plankton" (could be any microbe?), "egg" (might be larva or bird eggs?)
+# ter_animalcols  is all generic land animal samples, not clearly gut or organ, and most arthropods
+#                 includes mouse and rat, which may load it up with laboratory samples
+#                 includes 4 ambiguous terms" "invertebrate", "nematode", "parasite", "whole organism"
+# plantcols       is plants or plant-related materials, include roots, associated fungi, degradation of plant material
+# watercols       is all aquatic habitats, lakes, rivers, and frozen water bodies "ice" "snow" etc
+#                 includes "groundwater", "rock porewater", "hydrothermal vent", "cold seep" (could be with earth)
+# earthcols       is all land or earth sources
+#                 includes some ambiguous terms: "sediment", "marine sediment", "freshwater sediment", "alkali sediment"
+#                 "soil" (could be agricultural?), "peat" "bog" (could be aquatic)
+# industcols      is anything of manmade processes, environmental cleaning, industrial operations
+#                 also includes things related to petrol, fuels
+#                 includes "wastewater", "drinking water", "ballast water" (aquatic sources?)
+#                 and "cow dung", "manure" (could be with guts?)
+# electriccols    effectively a subset of industcols, things explicitly related to electrically active environments
+# citycols        is manmade environments, surfaces, and common objects
+# aircol          is air and related
+# microbecols     is some microbial terms like biofilms or mats, but also microbial interactions
+#                 includes some ambiguous terms: "eukaryotic plankton" (could be plant or animal?), "ecological" (no idea)
+# foodcols        is specific food processes or foodstuffs
+# plasticcols     is just to separate plastics from citycols
+# synthcols       is probably experimental/artificial microbial communities
+
 # set up color categories
 humancols = c("human", "human oral", "human nasopharyngeal", "human skin", "human vaginal", "human reproductive system", "human lung", "human milk", "human blood", "human tracheal", "human saliva", "human eye", "human bile", "human sputum", "human semen")
 gutscols = c("gut", "feces", "human gut", "mouse gut", "rat gut", "bovine gut", "pig gut", "sheep gut", "chicken gut", "insect gut", "fish gut", "invertebrate gut", "shrimp gut", "termite gut")
@@ -34,6 +65,7 @@ foodcols = c("food", "food production", "food fermentation", "honey", "wine", "p
 plasticcols = c("plastisphere", "plastic", "flotsam")
 synthcols = c("synthetic")
 
+# determine colors for all points
 colorvec = rep("#989898", length(metagenome_type))
 colorvec[which(!is.na(match(metagenome_type, humancols)))] = "#bf04a7"
 colorvec[which(!is.na(match(metagenome_type, gutscols)))] = "#ed9aea"
@@ -52,25 +84,35 @@ colorvec[which(!is.na(match(metagenome_type, foodcols)))] = "#de851b"
 # plasticcols stay gray
 # synthcols stay gray
 
+
+
+# draw the maps
+
+
 # for all categories
 # WARNING MAP IS LARGE
+# DUE TO APPX 600k POINTS
+# AND DOES NOT RENDER QUICKLY
+#
 #pdf( file="~/git/taxonomy_database/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon.pdf", height=12, width=24)
 #png( file="~/git/taxonomy_database/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon.png", height=1080, width=2160)
-#worldmap = map('world', fill=TRUE, col="#dedede", mar=c(0.1,0.1,0.1,0.1),)
-#points( longitude, latitude, bg=colorvec, pch=16, cex=1.5)
+#worldmap = map('world', fill=TRUE, col="#ededed", border="#898989", mar=c(0.1,0.1,0.1,0.1) )
+#points( longitude, latitude, bg=colorvec, pch=21, cex=1.5)
 #dev.off()
 
 
+
 # for individual terms
+# i.e. any term in the col categories above
 pdf( file="~/git/taxonomy_database/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon-sponge_only.pdf", height=12, width=24)
-worldmap = map('world', fill=TRUE, col="#dedede", mar=c(0.1,0.1,0.1,0.1),)
+worldmap = map('world', fill=TRUE, col="#ededed", border="#898989", mar=c(0.1,0.1,0.1,0.1) )
 spongeset = which(!is.na(match(metagenome_type, c("sponge") ) ) )
 points( longitude[spongeset], latitude[spongeset], bg="#9354cf88", pch=21, cex=3)
 dev.off()
 
-# for categories of terms, above
+# for whole categories of terms, above
 pdf( file="~/git/taxonomy_database/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon-plant_only.pdf", height=12, width=24)
-worldmap = map('world', fill=TRUE, col="#dedede", mar=c(0.1,0.1,0.1,0.1),)
+worldmap = map('world', fill=TRUE, col="#ededed", border="#898989", mar=c(0.1,0.1,0.1,0.1) )
 plantset = which(!is.na(match(metagenome_type, plantcols ) ) )
 points( longitude[plantset], latitude[plantset], bg="#18d02588", pch=21, cex=3)
 dev.off()
@@ -78,12 +120,12 @@ dev.off()
 # for overlay of multiple categories
 pdf( file="~/git/taxonomy_database/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon-sponge_water.pdf", height=12, width=24)
 #png( file="~/git/taxonomy_database/images/NCBI_SRA_Metadata_Full_20191130.metagenomes_latlon-sponge_water.png", height=540, width=1080)
-worldmap = map('world', fill=TRUE, col="#dedede", mar=c(0.1,0.1,0.1,0.1),)
+worldmap = map('world', fill=TRUE, col="#ededed", border="#898989", mar=c(0.1,0.1,0.1,0.1) )
 waterset = which(!is.na(match(metagenome_type, watercols ) ) )
 points( longitude[waterset], latitude[waterset], col="#45c5f488", pch=16, cex=1)
 spongeset = which(!is.na(match(metagenome_type, c("sponge") ) ) )
 points( longitude[spongeset], latitude[spongeset], bg="#9354cf88", pch=21, cex=3)
-legend(-130,-45, legend=c("Sponge", "Any water type"), col=c("#9354cf", "#45c5f4"), pch=16, cex=2)
+legend(-130,-45, legend=c("Sponge", "Any water type"), col=c("#9354cf", "#45c5f4"), pch=16, cex=2, bty='n')
 dev.off()
 
 

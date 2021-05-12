ac36514b7419fb37a7568b285c83bb83a55b1ff0
wfrancis <wfrancis@biology.sdu.dk>
2021-05-12 13:33:19 +0200

    error catch if no header and add virus kingdoms

--- taxon_barplot.R
+++ taxon_barplot.R
@@ -18,11 +18,13 @@ print(paste("Done",Sys.time()))
 
 kingdoms = sort(table(taxondata[["kingdom"]]),decreasing=FALSE)
 
-kingrefs = c( "None",   "Fungi",   "Metazoa", "Viridiplantae", "Bacteria", "Archaea")
-kingcols = c("#888888", "#4075b2", "#9354cf",  "#18d025",      "#c34741" , "#de851b")
+kingrefs = c( "None",   "Fungi",   "Metazoa", "Viridiplantae", "Bacteria", "Archaea", "Orthornavirae", "Pararnavirae", "Shotokuvirae", "Bamfordvirae", "Loebvirae", "Sangervirae" )
+kingcols = c("#888888", "#4075b2", "#9354cf",  "#18d025",      "#c34741" , "#de851b", "#aaaaaa",       "#aaaaaa",      "#aaaaaa",      "#aaaaaa",      "#aaaaaa",   "#aaaaaa")
 kingdomorder = match(names(kingdoms),kingrefs)
 
 totalspecies = sum(kingdoms)
+
+if (totalspecies==0){ print("ERROR: no species counted, try using --header with parse_ncbi_taxonomy.py") }
 
 xmax = max(pretty(max(kingdoms)))
 
@@ -42,10 +44,11 @@ fungiphyla = c("Ascomycota","Basidiomycota","Mucoromycota","Chytridiomycota")
 phaeophyla = c("Phaeophyceae","Apicomplexa")
 bactphyla = c("Proteobacteria", "Firmicutes", "Actinobacteria", "Bacteroidetes", "Spirochaetes", "Cyanobacteria", "Tenericutes", "Chloroflexi", "Chlamydiae", "Verrucomicrobia")
 archaphyla = c("Euryarchaeota", "Thaumarchaeota", "Crenarchaeota", "Parvarchaeota")
+virophyla = c("Pisuviricota", "Negarnaviricota", "Artverviricota", "Kitrinoviricota", "Uroviricota", "Peploviricota", "Cossaviricota", "Duplornaviricota", "Nucleocytoviricota")
 
 phylarefs = c(bactphyla,archaphyla,phaeophyla,fungiphyla,plantaphyla,metaphyla)
 # assumes "None" is first entry, so #888888
-phylacols = c("#888888",  rep(c("#c34741"),length(bactphyla)),  rep(c("#de851b"),length(archaphyla)),  rep(c("#8d8f0d"),length(phaeophyla)),  rep(c("#4075b2"),length(fungiphyla)),  rep(c("#18d025"),length(plantaphyla)),  rep(c("#9354cf"),length(metaphyla)) )
+phylacols = c("#888888",  rep(c("#c34741"),length(bactphyla)),  rep(c("#de851b"),length(archaphyla)),  rep(c("#8d8f0d"),length(phaeophyla)),  rep(c("#4075b2"),length(fungiphyla)),  rep(c("#18d025"),length(plantaphyla)),  rep(c("#9354cf"),length(metaphyla)), rep(c("#aaaaaa"),length(virophyla)) )
 phylaorder = match(names(phyla),phylarefs,nomatch=0)+1
 
 
@@ -72,7 +75,7 @@ print(paste("Generating .pdf",outputfile))
 pdf(file=outputfile, width=8, height=11)
 
 par(mar=c(1,10,3,1.6))
-layout(matrix(c(1,2),nrow=2),heights=c(1,4))
+layout(matrix(c(1,2),nrow=2),heights=c(1.2,4))
 bp1 = barplot(kingdoms, horiz=TRUE, las=1, xlim=c(0,xmax), col=kingcols[kingdomorder], main=inputfile, cex.lab=1.4, cex.axis=1.3)
 kt_positions = kingdoms/2
 kt_positions[kingdoms<xmax*0.06] = kingdoms[kingdoms<xmax*0.06]+xmax*0.05
